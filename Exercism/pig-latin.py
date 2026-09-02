789e151e9265d1f2ec8e34d6b77613dd8e00e734
LAVRA\laura <laura.sebestin@gmail.com>
2026-09-02 22:47:49 +0300

    fixed and improves pig latin solution

--- Exercism/pig-latin.py
+++ Exercism/pig-latin.py
@@ -1,39 +1,35 @@
+"""Module providing a function translating English into Pig Latin."""
 def translate(text):
-
+    """Function translating English into Pig Latin."""
     words = text.split()
     vowels = ["a", "e", "i", "o", "u"]
     new_words = []
+
     for word in words:
-        i = 0
         cons = ""
+        split_index = 0
         remaining_word = word
-
-        while word[i] not in vowels and word[i:i+2] != "qu":
-                print(word[i:i+2])
-                cons += word[i]
-                remaining_word = remaining_word.replace(word[i], "")
-                i += 1
-                print(remaining_word)
-
-        if remaining_word[:2] == "qu":
-            new_word = remaining_word[2:] + cons + "qu" + "ay"
-            new_words.append(new_word)
-            return " ".join(new_words)
 
         if word[0] in vowels or word[:2] == "xr" or word[:2] == "yt":
             new_word = word + "ay"
-            new_words.append(new_word)
-            return " ".join(new_words)
-                
-        if remaining_word[0] == "y":
-            y_index = word.find("y")
-            cons = word[:y_index + 1]
-            new_word = "y" + cons + word[y_index:]
-            new_words.append(new_word)
         else:
-            new_word = remaining_word + cons + "ay"
-            new_words.append(new_word)
-        return " ".join(new_words)
+            while word[split_index] not in vowels and word[split_index:split_index+2] != "qu":
+                split_index += 1
+                cons = word[:split_index]
+                remaining_word = word[split_index:]
+                if remaining_word[0] == "y" and cons != "":
+                    break
+
+        if remaining_word[:2] == "qu":
+            new_word = remaining_word[2:] + cons + "qu" + "ay"
+
+        elif remaining_word[0] == "y":
+            new_word = "y" + remaining_word[1:] + cons + "ay"
+        else:
+            new_word = remaining_word + cons + "ay"      
+
+        new_words.append(new_word)
+    return " ".join(new_words)
 
 if __name__ == "__main__":
-    print(translate("square"))
+    print(translate("chair"))
