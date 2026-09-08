775618d05ae8569b25eca0284afaa177d57729ed
LAVRA\laura <laura.sebestin@gmail.com>
2026-09-08 22:22:47 +0300

    started treasure exercise

---
+++ Exercism/tisbury-treasure-hunt.py
@@ -0,0 +1,46 @@
+"""Functions to help Azara and Rui locate pirate treasure."""
+
+
+def get_coordinate(record):
+    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate."""
+    return record[1]
+
+def convert_coordinate(coordinate):
+    """Split the given coordinate into tuple containing its individual components."""
+    return tuple(coordinate)
+
+def compare_records(azara_record, rui_record):
+    """Compare two record types and determine if their coordinates match."""
+    return tuple(azara_record[1]) == rui_record[1]
+
+def create_record(azara_record, rui_record):
+    """Combine the two record types (if possible) and create a combined record group."""
+    if compare_records(azara_record, rui_record):
+        return azara_record + rui_record
+    return "not a match"
+
+def clean_up(combined_record_group):
+    """Clean up a combined record group into a multi-line string of single records."""
+    result = ""
+    for record in combined_record_group:
+        group_list = list(record)
+        group_list.pop(1)
+        strg = ", ".join(group_list)
+        result = result + "({}\n)".format(strg)
+
+    return result
+
+if __name__ == "__main__":
+    print(clean_up((('Scrimshawed Whale Tooth', '2A', 'Deserted Docks', ('2', 'A'), 'Blue'),
+                ('Brass Spyglass', '4B', 'Abandoned Lighthouse', ('4', 'B'), 'Blue'),
+                ('Robot Parrot', '1C', 'Seaside Cottages', ('1', 'C'), 'Blue'),
+                ('Glass Starfish', '6D', 'Tangled Seaweed Patch', ('6', 'D'), 'Orange'),
+                ('Vintage Pirate Hat', '7E', 'Quiet Inlet (Island of Mystery)', ('7', 'E'), 'Orange'),
+                ('Pirate Flag', '7F', 'Windswept Hilltop (Island of Mystery)', ('7', 'F'), 'Orange'),
+                ('Crystal Crab', '6A', 'Old Schooner', ('6', 'A'), 'Purple'),
+                ('Model Ship in Large Bottle', '8A', 'Harbor Managers Office', ('8', 'A'), 'Purple'),
+                ('Angry Monkey Figurine', '5B', 'Stormy Breakwater', ('5', 'B'), 'Purple'),
+                ('Carved Wooden Elephant', '8C', 'Foggy Seacave', ('8', 'C'), 'Purple'),
+                ('Amethyst  Octopus', '1F', 'Aqua Lagoon (Island of Mystery)', ('1', 'F'), 'Yellow'),
+                ('Antique Glass Fishnet Float', '3D', 'Spiky Rocks', ('3', 'D'), 'Yellow'),
+                ('Silver Seahorse', '4E', 'Hidden Spring (Island of Mystery)', ('4', 'E'), 'Yellow'))))
