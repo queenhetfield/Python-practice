0104c7b1d07b01a65df2c618fd83fb8018cfb79c
LAVRA\laura <laura.sebestin@gmail.com>
2026-09-11 16:12:32 +0300

    finished managment

--- Exercism/macha-munc-managment.py
+++ Exercism/macha-munc-managment.py
@@ -1,36 +1,32 @@
 """Functions to manage a users shopping cart items."""
-
-
 def add_item(current_cart, items_to_add):
     """Add items to shopping cart."""
     for item in items_to_add:
         current_cart.setdefault(item, 0)
         current_cart[item] += 1
     return current_cart
-
 def read_notes(notes):
     """Create user cart from an iterable notes entry."""
     return dict.fromkeys(notes, 1)
-
 def update_recipes(ideas, recipe_updates):
     """Update the recipe ideas dictionary."""
     ideas |= recipe_updates
     return ideas
-    
-
 def sort_entries(cart):
     """Sort a user's shopping cart in alphabetical order."""
     sorted_cart = dict(sorted(cart.items()))
     return sorted_cart
-
 def send_to_store(cart, aisle_mapping):
     """Combine user's order to aisle and refrigeration information."""
     sorted_cart = dict(sorted(cart.items(), reverse=True))
     for name, count in sorted_cart.items():
         sorted_cart[name] = [count, *aisle_mapping[name]]
     return sorted_cart
-        
-
 def update_store_inventory(fulfillment_cart, store_inventory):
     """Update store inventory levels with user order."""
-
+    for item, info in fulfillment_cart.items():
+        if item in store_inventory:
+            store_inventory[item][0] -= info[0]
+            if store_inventory[item][0] <= 0:
+                store_inventory[item][0] = "Out of Stock"
+    return store_inventory
