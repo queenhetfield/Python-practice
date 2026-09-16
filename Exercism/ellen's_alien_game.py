a35ec5159c573325c20368f764d0efbf071410df
LAVRA\laura <laura.sebestin@gmail.com>
2026-09-16 20:05:51 +0300

    started alien game

---
+++ Exercism/ellen's_alien_game.py
@@ -0,0 +1,13 @@
+"""Solution to Ellen's Alien Game exercise."""
+
+
+class Alien:
+    """Create an Alien object with location x_coordinate and y_coordinate."""
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.health = 3
+
+
+#TODO (Student): Create the new_aliens_collection() function below to call your Alien class with a list of coordinates
+
