ccf39bfed029df4f1c0464a621417b21b952bfeb
LAVRA\laura <laura.sebestin@gmail.com>
2026-09-17 22:23:25 +0300

    finished alien game

--- Exercism/ellen's_alien_game.py
+++ Exercism/ellen's_alien_game.py
@@ -1,13 +1,32 @@
 """Solution to Ellen's Alien Game exercise."""
-
-
 class Alien:
+    """Class representing a person"""
+    total_aliens_created = 0
+    
     """Create an Alien object with location x_coordinate and y_coordinate."""
     def __init__(self, x_coordinate, y_coordinate):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
         self.health = 3
+        Alien.total_aliens_created += 1
+        
+    def hit(self):
+        if self.health > 0:
+            self.health -= 1
+    def is_alive(self):
+        return self.health != 0
+        
+    def teleport(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
 
-
-#TODO (Student): Create the new_aliens_collection() function below to call your Alien class with a list of coordinates
-
+    def collision_detection(self, other_object):
+        pass
+    
+def new_aliens_collection(positions):
+    """Function to call Alien class with a list of coordinates"""
+    alien_objects = []
+    for position in positions:
+        alien_objects.append(Alien(*position))
+    return alien_objects
+    
